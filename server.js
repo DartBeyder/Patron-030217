9e692e807ca4850271eed3eb81d1a93b85bfd66f
DartBeyder <jepatron0619@gmail.com>
2017-03-02 20:33:45 +0800

    YO

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ app.get('/', function(req, res){
 app.get('/about', function(req, res){
   res.sendFile(path.join(__dirname, 'views/about.html'));
 });
+app.get('/json', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/json.html'));
+});
 app.get('*', function(req, res){
   res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
 });
+
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
